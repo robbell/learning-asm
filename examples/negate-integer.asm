f44d88ffa1c879e3c5aa89c3cb48fb8d4b8d76dd
Rob Bell <me@rob-bell.net>
2020-05-15 20:35:13 +0100

    Add ADDIU variant for negation

--- examples/negate-integer.asm
+++ examples/negate-integer.asm
@@ -4,11 +4,18 @@
         .globl  main
 
 main:
-        ori      $7, $0, 146 # Put +146 into $7
-        ori      $8, $0,  82 # Put 82 into $8
-        nor      $8, $8,  $0 # Reflect
-        ori      $9, $0,   1 # Load 1 into a register
-        addu     $8, $8,  $9 # Add 1: $8 = -82
-        addu    $10, $7,  $8 # (+146) + (-82)
+        ori      $7,  $0, 146 # Put +146 into $7
+        ori      $8,  $0,  82 # Put 82 into $8
+        nor      $8,  $8,  $0 # Reflect
+
+# Using addu
+        ori      $9,  $0,   1 # Load 1 into a register
+        addu     $10, $8,  $9 # Add 1: $8 = -82
+
+# Using addiu
+        addiu    $10, $8,   1 # Add 1: $8 = -82
+
+# Total
+        addu     $11, $7,  $10 # (+146) + (-82)
 
 ## End of file
