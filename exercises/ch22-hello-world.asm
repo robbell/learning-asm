f4c486ce422310021a0288d53f582b66ecb063c2
Rob Bell <me@rob-bell.net>
2020-05-26 22:45:38 +0100

    Add 'Hello, World' example, at last

---
+++ exercises/ch22-hello-world.asm
@@ -0,0 +1,14 @@
+# Hello, World!
+#
+        .text
+        .globl  main
+main:
+        li      $v0, 4      # code 4 == print string
+        la      $a0, string # $a0 == address of the string
+        syscall             # Invoke the exception handler.
+
+        li      $v0, 10     # code 10 == exit
+        syscall             # Halt the program.
+
+        .data
+string: .asciiz      "Hello, World!\n"
