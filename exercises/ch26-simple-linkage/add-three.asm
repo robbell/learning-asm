949097c232831c4bab7fda2fb83f5bb0c473b66a
Rob Bell <me@rob-bell.net>
2020-06-03 22:53:22 +0100

    Add subroutine call via global symbol

--- exercises/ch26-simple-linkage/add-three.asm
+++ exercises/ch26-simple-linkage/add-three.asm
@@ -31,30 +31,3 @@ main:
         .data
 heading:
         .asciiz "The sum is: "
-
-
-# pread -- prompt for and read an integer
-#
-# on entry:
-#    $ra -- return address
-#
-# on exit:
-#    $v0 -- the integer
-
-pread:
-        la      $a0, prompt         # print string
-        li      $v0, 4              # service 4
-        syscall
-
-        li      $v0, 5              # read int into $v0
-        syscall                     # service 5
-
-        jr      $ra
-
-    # return
-
-        nop                         # branch delay slot
-
-        .data
-prompt:
-        .asciiz "Enter an integer"
