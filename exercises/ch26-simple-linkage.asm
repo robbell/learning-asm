7cb9064a4d505077dbcf91af69e09877de36bcb2
Rob Bell <me@rob-bell.net>
2020-06-03 22:42:56 +0100

    Add call to subroutine

--- exercises/ch26-simple-linkage.asm
+++ exercises/ch26-simple-linkage.asm
@@ -1,3 +1,38 @@
+        .text
+        .globl  main
+
+main:
+        jal     pread           # read first integer
+        nop                     #
+        move    $s0, $v0        # save it in $s0
+
+        jal     pread           # read second integer
+        nop                     #
+        move    $s1, $v0        # save it in $s1
+
+        jal     pread           # read third integer
+        nop                     #
+        move    $s2, $v0        # save it in $s2
+
+        addu    $s0, $s0, $s1   # compute the sum
+        addu    $s3, $s0, $s2   # result in $s3
+
+        li      $v0, 4          # print a heading
+        la      $a0, heading
+        syscall
+
+        move    $a0, $s3        # move sum into parameter
+        li      $v0, 1          # print the sum
+        syscall
+
+        li      $v0, 10         # exit
+        syscall
+
+        .data
+heading:
+        .asciiz "The sum is: "
+
+
 # pread -- prompt for and read an integer
 #
 # on entry:
