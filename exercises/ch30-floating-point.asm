6ca7cd79079d8d3d9f880d4c8ffcfc7ab657a59a
Rob Bell <me@rob-bell.net>
2020-06-08 21:26:03 +0100

    Add comment showing sections of an IEEE float

--- exercises/ch30-floating-point.asm
+++ exercises/ch30-floating-point.asm
@@ -3,5 +3,7 @@
         .globl  main
 main:
         .data
-val:    .float    1.0 # 0x3F800000
+val:    .float    1.0 # 0x3F800000 or 0|01111111|00000000000000000000000
+                      #               ^    ^                  ^
+                      #             sign exponent          mantissa
 ## End of file
